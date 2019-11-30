63aa9b5abba27783c12d8f00598daf6f68bd9bdd
pankaj-pant <pankajpant89@gmail.com>
2019-11-30 11:06:30 +0200

    cleared junk files

--- src/PersonalTrainerApp.js
+++ src/PersonalTrainerApp.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import './PersonalTrainerApp.css'
-import './Customers.css'
 import AddTrainingForm from './AddTrainingForm'
 import useToggleState from './hooks/useToggleState'
 
