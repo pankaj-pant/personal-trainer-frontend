15dd392d24d1575ffee1d4aace67daa52ff4474f
pankaj-pant <pankajpant89@gmail.com>
2019-11-30 21:34:56 +0200

    nav bar color

--- src/App.js
+++ src/App.js
@@ -22,8 +22,6 @@ function App() {
           </Switch>
         </div>
       </Router>
-
-
     </div>
   )
 }
--- src/Navigator.js
+++ src/Navigator.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Navigator() {
   return(
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <button className="navbar-toggler navbar-toggler-right" type="button"
         data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false"
